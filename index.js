ba1edfdbe0231fa13c07d0c590c650239bd270ab
Karan Shrestha <iamkaranshrestha96@gmail.com>
2019-11-26 20:03:23 +0100

    working server

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const path = require("path")
+
+
+
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.get('/', (req, res) => {
+    console.log("i hear you")
+
+    res.sendFile(path.join(__dirname, "frame/home.html"))
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
